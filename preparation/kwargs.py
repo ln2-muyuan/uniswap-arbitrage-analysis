ba1e77d21b5398658cdf60de4f8e746240ac2481
Mu'Ye <lmy2019@outlook.com>
2022-12-07 14:29:36 +0800

    Update

--- preparation/kwargs.py
+++
@@ -1,9 +0,0 @@
-def team(*members, **features):
-    print(members)
-    print(features)
-
-    print(*members)
-    print(*features)
-
-
-team('John', 'Paul', 'George', 'Ringo', manager='Brian', drummer='Ringo', name="The Beatles")
